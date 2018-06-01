fe1ebc44587582f6bf98c044739674d59a87f62a
looper123 <m17701848923@163.com>
2018-06-01 16:29:52 +0800

    explain

--- com/quark/arithmetic/K-neighbour/kNN.py
+++ com/quark/arithmetic/K-neighbour/kNN.py
@@ -1,9 +1,14 @@
+from os import listdir
+
 from numpy import *
 import operator
+import matplotlib
+import matplotlib.pyplot as plt
 
+# k-近邻算法
 
 # 待分类的数据   训练样本集   标签向量（类别）   选择近邻的数目
-def clssfify0(inX, dataSet, labels, k):
+def classify0(inX, dataSet, labels, k):
     # 计算已知类别的数据集中的点与当前点之间的距离 start
     dataSetSize = dataSet.shape[0]
     # 把待分类数据复制四份 组成二维数组的形式 并和训练样本集相减 得到输入数据和各个样本集x，y的差值
@@ -12,6 +17,7 @@ def clssfify0(inX, dataSet, labels, k):
     #  [0 0]            [0.   0.]
     #  [0 0]]           [0. - 0.1]]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    # 使用欧式距离公式计算（x0,y0）和（x1,y1）两个点间的最短距离公式    d= √(x0-x1)²+（y0-y1）²
     # 求各组距离的平方 [x²，y²]
     # [[1.   1.21]
     #  [1.   1.]
@@ -19,25 +25,125 @@ def clssfify0(inX, dataSet, labels, k):
     # [0.   0.01]]
     sqDiffMat = diffMat ** 2
     # 各组平方相加 [x²+y²]
-    # [2.21  2.  0.  0.01]
+    # [2.21
+    #    2.
+    #    0.
+    #    0.01]
     sqDistance = sqDiffMat.sum(axis=1)
     # 求平方根
     distances = sqDistance ** 0.5
     # 计算已知类别的数据集中的点与当前点之间的距离 end
     # 选择距离最小的k个点 start
     # 对距离进行排序
-    # [2 3 1 0]
+    # [2 3 1 0]  返回从小到大的索引值
     sortedDistIndicies = distances.argsort()
     classCount = {}
     # 挑选邻近的k个点
     for i in range(k):
-        # 使用欧式距离公式计算（x0,y0）和（x1,y1）两个点间的最短距离公式    d= √(x0-x1)²+（y0-y1）²
         voteIlabel = labels[sortedDistIndicies[i]]
+        # 邻近的点按分类叠加放入 classCount -->  classCount {'A':1,'B':2}
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     # 选择距离最小的k个点 end
-    # 排序
+    # 排序 获取票数最高的分类 得到该点所属的分类
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+
+# read data from file
+def files2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelsVector = []
+    index = 0
+    for line in arrayOLines:
+        line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] =listFromLine[0:3]
+        classLabelsVector.append(int(listFromLine[-1]))
+        index +=1
+    return returnMat,classLabelsVector
+
+
+# 在使用欧式距离公式计算距离的时候 可能出现两个属性之间之间差值较大的情况 ，这会使该属性对计算结果的影响远远大于其他属性（而现实情况是所有属性同等重要）如何处理？？
+# 使用归一化数值  公式  newValue = (oldValue - minVal)/(maxVal - minVal)  把所有原始数据取值范围处理到0~1 或者 -1~0之间提高最后结果的精确度
+# 训练数据归一化
+def autoNorm(dataSet):
+    # 每列最小值
+    minVals = dataSet.min(0)
+    # 每列最大值
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    # 因为特征值（训练数据）组成的矩阵是1*3 列的矩阵 所以minVals 和 range的值也必须为 1*3 列的矩阵 （不然无法做数学计算）
+    normDataSet = dataSet-tile(minVals,(m,1))
+    normDataSet = normDataSet/tile(ranges,(m,1))  #note that:  '/' in this place means  特征值相除而不是矩阵除法  在numpy包中要使用矩阵除法：linalg.solve(matA,matB)
+    return normDataSet,ranges,minVals
+
+
+# 分类器对约会网站的测试代码
+def datingClassTest():
+    horatio = 0.10
+    # read data from file
+    datingDataMat,datingLabels = files2matrix('datingTestSet.txt')
+    # 数据归一化
+    normMat,ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*horatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        # 待处理数据分类
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print("classifyResult:%d;realAnwer:%d"%(classifierResult,datingLabels[i]))
+        # 结果正确性判断
+        if(classifierResult !=datingLabels[i]):errorCount +=1.0
+    # 计算错误率
+    print("total error rate is :%f"%(errorCount/float(numTestVecs)))
+
+
+
+    # 手写识别系统
+    # 把图像转化为向量  创建 1*1024 的numpy数组  打开给定文件 循环读出文件前32行 并把每行头32个字符存储在numpy数组中 返回该数组
+    def img2Vector(filename):
+        returnVect = zeros((1,1024))
+        fr = open(filename)
+        for i in range(32):
+            lineStr = fr.readline()
+            for j in range(32):
+                returnVect[0,32*i+j] = int(lineStr[j])
+        return returnVect
+
+
+    # 使用k近邻算法识别手写数字
+    def  handWritingClassTest():
+        hwLabels = []
+        # 获取目录内容
+        trainingFileList = listdir('trainingDigits')
+        m = len(trainingFileList)
+        trainingMat = zeros((m,1024))
+        for i in range(m):
+            # 从文件名解析分类数字
+            fileNameStr = trainingFileList[i]
+            fileStr = fileNameStr.split('.')[0]
+            classNumStr = int(fileStr.split('_')[0])
+            hwLabels.append(classNumStr)
+            trainingMat[i:]  = img2Vector('traningDigits/%s'%fileNameStr)
+        testFileList = listdir('testDigits')
+        errorCount = 0.0
+        mTest = len(testFileList)
+        for i in range(mTest):
+            fileNameStr = testFileList[i]
+            fileStr = fileNameStr.split('.')[0]
+            classNumStr = int(fileStr.split('_')[0])
+            vectorUnderTest = img2Vector('testDigits%s'%fileNameStr)
+            classifierResult = classify0(vectorUnderTest,trainingMat,hwLabels,3)
+            print("classifyResult:%d;realAnwer:%d" % (classifierResult, datingLabels[i]))
+            # 结果正确性判断
+            if (classifierResult != classNumStr): errorCount += 1.0
+        # 错误率
+        print("total error rate is :%f" % (errorCount / float(mTest)))
 
 
 def createDataset():
@@ -47,9 +153,36 @@ def createDataset():
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+def myKnnArithMetic(orginData ,orginResult ,testData ,k):
+    size = orginData.shape[0]
+    # 获取最大 最小值
+    maxData = group.max(0)
+    minData = group.min(0)
+    # 训练数据归一化
+    normalData =( group-tile(minData,(size,1)))/(tile(maxData-minData,(size,1)))
+    difValSquare = (tile(testData,(size,1))-normalData)**2
+    difValSum = difValSquare.sum(axis=1)
+    difValSqrt = difValSum**0.5
+    indexOrder = difValSqrt.argsort()
+    rate = {}
+    for i in range(k):
+        # 存在+1 不存在初始化为0
+        key =  orginResult[indexOrder[i]]
+        rate[key] = rate.get(key,0)+1
+    #获取属性占比最多的数据
+    sortedDict = sorted(rate, key=operator.itemgetter(0),reverse=True)
+    return sortedDict[0][0]
+
+
+
 
 if __name__ == '__main__':
-    group, labels = createDataset()
-    result = clssfify0([0, 0], group, labels, 3)
-    print(result)
+    # group, labels = createDataset()
+    # result = classify0([0, 0], group, labels, 3)
+    # print(result)
+    # print(sorted(group,key= lambda a: a[0],reverse= True)[0][0])
+    group = array([[100.0, 123.1], [131.0, 161.0], [140, 200], [100, 212.1],[147,174]])
+    myData = [150,150]
+    labels = ['A','B','A','B','B']
+    print(myKnnArithMetic(group,labels,myData,4))
 
---
+++ com/quark/arithmetic/decision-tree/DST.py
@@ -0,0 +1,259 @@
+import operator
+from math import log
+
+import matplotlib.pyplot as plt
+
+
+# 决策树
+
+# 求给定数据集的香农熵  note:当dataset中有一种类型的数据时 熵为0  当存在两种类型的数据 并且数量相同时 熵为1
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}  # {'yes': 2, 'no': 3}
+    # 为所有分类创建字典 start
+    for featVec in dataSet:
+        currentlabel = featVec[-1]
+        if currentlabel not in labelCounts.keys():
+            # 如果分类不在labelCounts 中 就添加进去
+            labelCounts[currentlabel] = 0
+        labelCounts[currentlabel] += 1
+    # 为所有分类创建字典 end
+    shannonEnt = 0.0
+    for key in labelCounts:
+        # labelCounts中各分类占总分类的比例
+        prob = float(labelCounts[key] / numEntries)
+        # 求以2为底的对数
+        #                c
+        # Entropy(S) =   Σ -pi log2pi
+        #               i=1
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+# 数据
+def createDataSet():
+    dataSet = [
+        [1, 1, 'yes'],
+        [1, 1, 'yes'],
+        [1, 0, 'no'],
+        [0, 1, 'no'],
+        [1, 1, 'no']
+    ]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+# 按照给定的特征划分数据集:找到对应角标的属性值==value数据 并且删掉该角标对应的属性值 最后输出
+# 参数            数据集  属性对应的角标 属性值
+# [[1, 'yes'], [1, 'yes'], [0, 'no'], [1, 'no']]
+def splitDataSet(dataset, axis, value):
+    retDataSet = []
+    for featVec in dataset:
+        if featVec[axis] == value:
+            reduceFeatVec = featVec[:axis]
+            reduceFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reduceFeatVec)
+    return retDataSet
+
+
+# 求最大增益属性对应的角标
+def chooseMaxBenefitProperty(dataset):
+    numFeatures = len(dataset[0]) - 1
+    # 香农熵
+    baseEntroy = calcShannonEnt(dataset)
+    bestInfoGain = 0.0;
+    bestFeature = -1
+    for i in range(numFeatures):
+        # 获取训练集中的某一属性的所有值比如dataset中第一个属性 [1,1,1,0,1]组成元组
+        featList = [example[i] for example in dataset]
+        # 去重复 [0,1]
+        uniqueVals = set(featList)
+        newEntroy = 0.0
+        for value in uniqueVals:
+            # 按照给定的特征划分数据集
+            subDataSet = splitDataSet(dataset, i, value)
+            # 求该特征集占总量的比例
+            prob = len(subDataSet) / float(len(dataset))
+            newEntroy += prob * calcShannonEnt(subDataSet)
+        # 该属性的增益
+        infoGain = baseEntroy - newEntroy
+        # 最大增益属性
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            # 最大增益属性对应的角标
+            bestFeature = i
+    return bestFeature
+
+
+# 当数据集已经处理了所有属性，但是类标签已然不是唯一的，如何定义该叶子节点
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+# 使用dict类型来模拟决策树结构
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    # 类别相同停止划分
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 遍历完所有特征时返回出现次数最多的类别
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    # 最大增益
+    bestFeat = chooseMaxBenefitProperty(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
+# 决策节点文本框格式
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+# 叶子节点文本框格式
+leafNode = dict(boxstyle="round4", fc="0.8")
+# 箭头格式
+arrow_args = dict(arrowstyle="<-")
+# matplotlib中文乱码
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+
+# matplotlib绘图测试
+def createPlotTest():
+    # x和y轴长度   颜色
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    createPlotTest.axl = plt.subplot(111, frameon=False)
+    # 节点名称  节点坐标
+    plotNode('决策节点', (0.5, 0.1), (0.1, 0.5), decisionNode)
+    plotNode('叶子节点', (0.8, 0.1), (0.3, 0.8), leafNode)
+    plt.show()
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.axl.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', xytext=centerPt, textcoords='axes fraction',
+                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+
+# 获取叶子节点的数目 (叶子节点:已经获得最终分类结果 )
+def getNumLeaf(myTree):
+    numLeaf = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        # 如果该节点下还有数据 -> 递归
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeaf += getNumLeaf(secondDict[key])
+        else:
+            numLeaf += 1
+    return numLeaf
+
+
+# 获取树的层数
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        # 取最高的层数
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+    return maxDepth
+
+
+# 决策树绘图数据准备
+def retrieveTree(i):
+    listOfTrees = [{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+                   {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}]
+    return listOfTrees[i]
+
+
+# 父子节点填充文本信息
+def plotMidText(cntrpt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrpt[0]) / 2.0 + cntrpt[0]
+    yMid = (parentPt[1] - cntrpt[1]) / 2.0 + cntrpt[1]
+    createPlot.axl.text(xMid, yMid, txtString)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeaf(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+
+# 创建决策树
+def createPlot(intree):
+    # create figure
+    flg = plt.figure(1, facecolor='white')
+    flg.clf()
+    axprops = dict(xticks=[], yticks=[])
+    # 创建坐标轴
+    createPlot.axl = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeaf(intree))
+    plotTree.totalD = float(getTreeDepth(intree))
+    plotTree.xOff = -0.5 / plotTree.totalW;
+    plotTree.yOff = 1.0
+    plotTree(intree, (0.5, 1.0), '')
+    plt.show()
+
+
+# 使用决策树对数据进行分类
+def classify(inputTree, featLabels, testVec):
+    firstStr = list(inputTree.keys())[0]
+    secondDict = inputTree[firstStr]
+    # 这里假设测试数据中的属性排列顺序和 label 中一致
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict:
+        if testVec[featIndex] ==key:
+            if type(secondDict[key]).__name__ =='dict':
+                classLabel = classify(secondDict[key],featLabels,testVec)
+            else: classLabel = secondDict[key]
+    return classLabel
+
+
+
+
+if __name__ == '__main__':
+    myDat, labels = createDataSet()
+    # 求数据集熵值
+    print(calcShannonEnt(myDat))
+    # 属性分类
+    # print(splitDataSet(myDat,0,1))
+    # 最大增益属性
+    print(chooseMaxBenefitProperty(myDat))
+    # 创建tree的dict结构
+    print(createTree(myDat, labels))
+    # matplotlib绘图测试
+    myTree = retrieveTree(0)
+    # myTree1 = retrieveTree(1)
+    # print(getTreeDepth(myTree))
+    # createPlot(myTree1)
+    # print(matplotlib.matplotlib_fname())  #获取matplotlib包所在路径
+    # print(classify(myTree,labels,[1,0]))
